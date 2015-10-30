a670d97c6fe7672c3310b9b52aa490adb75bae79
Peter Wildani <pabw00@gmail.com>
2015-10-30 08:45:41 -0700

    Bit read tests.

--- src/protocol/bits.rs
+++ src/protocol/bits.rs
@@ -17,11 +17,16 @@ pub trait MutData: Data {
 
 impl Data for [u8] {
     type Slice = [u8];
-    #[inline] fn get(&self, index: usize) -> Option<&u8> { (self as &[u8]).get(index) }
+    #[inline]
+    fn get(&self, index: usize) -> Option<&u8> {
+        (self as &[u8]).get(index)
+    }
 
     #[inline]
     fn get_range(&self, index: Range<usize>) -> Option<&Self::Slice> {
-        if index.end > self.len() { return None; }
+        if index.end > self.len() {
+            return None;
+        }
         Some(&self[index])
     }
 }
@@ -29,11 +34,16 @@ impl Data for [u8] {
 impl MutData for [u8] {
     type SliceMut = [u8];
 
-    #[inline] fn get_mut(&mut self, index: usize) -> Option<&mut u8> { (self as &mut[u8]).get_mut(index) }
+    #[inline]
+    fn get_mut(&mut self, index: usize) -> Option<&mut u8> {
+        (self as &mut [u8]).get_mut(index)
+    }
 
     #[inline]
     fn get_mut_range(&mut self, index: Range<usize>) -> Option<&mut Self::SliceMut> {
-        if index.end > self.len() { return None; }
+        if index.end > self.len() {
+            return None;
+        }
         Some(&mut self[index])
     }
 }
@@ -43,15 +53,20 @@ type U8Slice = [u8];
 impl Data for Vec<u8> {
     type Slice = [u8];
 
-    #[inline] fn get(&self, index: usize) -> Option<&u8> { U8Slice::get(self, index) }
+    #[inline]
+    fn get(&self, index: usize) -> Option<&u8> {
+        U8Slice::get(self, index)
+    }
 
     #[inline]
     fn get_range(&self, index: Range<usize>) -> Option<&Self::Slice> {
-        if index.end > self.len() { return None; }
+        if index.end > self.len() {
+            return None;
+        }
         Some(&self[index])
     }
 }
-    
+
 /// Bit field manipulation. Does not yet work across byte boundaries.
 pub struct BitField {
     /// Byte offset from start of data.
@@ -80,27 +95,28 @@ impl BitField {
     #[inline]
     pub fn set<T: MutData + ?Sized>(&self, data: &mut T, value: u8) {
         if let Some(elem) = data.get_mut(self.index) {
-            *elem = (*elem & !self.mask) | ((value as u8 & self.mask) << self.mask.trailing_zeros());
+            *elem = (*elem & !self.mask) |
+                    ((value as u8 & self.mask) << self.mask.trailing_zeros());
         }
     }
 }
 
 pub struct BEU16Field {
-    pub index: usize
+    pub index: usize,
 }
 
 impl BEU16Field {
     #[inline]
     pub fn get<T: Data + ?Sized>(&self, data: &T) -> Option<u16> {
-        if let Some(split) = data.get_range(self.index..self.index+2) {
-            return Some((split[0] as u16) << 8 + (split[1] as u16));
+        if let Some(split) = data.get_range(self.index..self.index + 2) {
+            return Some(((split[0] as u16) << 8) + (split[1] as u16));
         }
         None
     }
 
     #[inline]
     pub fn set<T: MutData + ?Sized>(&self, data: &mut T, value: u16) {
-        if let Some(split) = data.get_mut_range(self.index..self.index+2) {
+        if let Some(split) = data.get_mut_range(self.index..self.index + 2) {
             split[0] = ((value & 0xff00) >> 8) as u8;
             split[1] = (value & 0x00ff) as u8;
         }
@@ -108,17 +124,15 @@ impl BEU16Field {
 }
 
 pub struct BEU32Field {
-    pub index: usize
+    pub index: usize,
 }
 
 impl BEU32Field {
     #[inline]
     pub fn get<T: Data + ?Sized>(&self, data: &T) -> Option<u32> {
-        if let Some(split) = data.get_range(self.index..self.index+4) {
-            return Some(((split[0] as u32) << 24) +
-                        ((split[1] as u32) << 16) +
-                        ((split[2] as u32) << 8) +
-                        ((split[3] as u32) << 0))
+        if let Some(split) = data.get_range(self.index..self.index + 4) {
+            return Some(((split[0] as u32) << 24) + ((split[1] as u32) << 16) +
+                        ((split[2] as u32) << 8) + ((split[3] as u32) << 0));
         }
         None
     }
@@ -126,7 +140,7 @@ impl BEU32Field {
     #[inline]
     pub fn set<T: MutData + ?Sized>(&self, data: &mut T, value: u32) {
         // TODO unsafe impl, once I can tell what the native endianness is.
-        if let Some(split) = data.get_mut_range(self.index..self.index+4) {
+        if let Some(split) = data.get_mut_range(self.index..self.index + 4) {
             split[0] = ((value & 0xff000000) >> 24) as u8;
             split[1] = ((value & 0x00ff0000) >> 16) as u8;
             split[2] = ((value & 0x0000ff00) >> 8) as u8;
@@ -139,26 +153,121 @@ impl BEU32Field {
 #[cfg(test)]
 mod tests {
     use super::BitField;
+    use super::BEU16Field;
+    use super::BEU32Field;
 
     #[test]
-    fn bits0() {
+    fn u8_extract0() {
         let data = [0xab, 0xcd];
-        let view:&[u8] = &data[..];
-        let field = BitField{index:0, mask:0xf0};
-        assert_eq!(Some(0xa), field.get(view));
+        let view: &[u8] = &data[..];
+        let field = BitField {
+            index: 0,
+            mask: 0xff,
+        };
+        assert_eq!(Some(0xab), field.get(view));
     }
+
     #[test]
-    fn bits1() {
+    fn u8_extract1() {
         let data = [0xab, 0xcd];
-        let view:&[u8] = &data[..];
-        let field = BitField{index:1, mask:0xf0};
+        let view: &[u8] = &data[..];
+        let field = BitField {
+            index: 1,
+            mask: 0xff,
+        };
+        assert_eq!(Some(0xcd), field.get(view));
+    }
+
+    #[test]
+    fn u4_extract() {
+        let data = [0xab, 0xcd];
+        let view: &[u8] = &data[..];
+        let field = BitField {
+            index: 1,
+            mask: 0xf0,
+        };
         assert_eq!(Some(0xc), field.get(view));
     }
+
     #[test]
-    fn bits1() {
+    fn u1_extract() {
         let data = [0xab, 0xcd];
-        let view:&[u8] = &data[..];
-        let field = BitField{index:1, mask:0xf0};
-        assert_eq!(Some(0xc), field.get(view));
+        let view: &[u8] = &data[..];
+        let field = BitField {
+            index: 1,
+            mask: 0x80,
+        };
+        assert_eq!(Some(0x1), field.get(view));
+    }
+
+    #[test]
+    fn bitfield_invalid_adddress() {
+        let data = [0xab];
+        let view: &[u8] = &data[..];
+        let field = BitField {
+            index: 1,
+            mask: 0xf0,
+        };
+        assert_eq!(None, field.get(view));
+    }
+
+    #[test]
+    fn u16_extract() {
+        let data = [0xab, 0xcd];
+        let view: &[u8] = &data[..];
+        let field = BEU16Field {
+            index: 0,
+        };
+        assert_eq!(Some(0xabcd), field.get(view));
+    }
+
+    #[test]
+    fn u16_unaligned() {
+        let data = [0xab, 0xcd, 0xef];
+        let view: &[u8] = &data[..];
+        let field = BEU16Field {
+            index: 1,
+        };
+        assert_eq!(Some(0xcdef), field.get(view));
+    }
+
+    #[test]
+    fn u16_invalid_address() {
+        let data = [0xab, 0xcd];
+        let view: &[u8] = &data[..];
+        let field = BEU16Field {
+            index: 1,
+        };
+        assert_eq!(None, field.get(view));
+    }
+
+    #[test]
+    fn u32_extract() {
+        let data = [0xab, 0xcd, 0xef, 0x01];
+        let view: &[u8] = &data[..];
+        let field = BEU32Field {
+            index: 0,
+        };
+        assert_eq!(Some(0xabcdef01), field.get(view));
+    }
+
+    #[test]
+    fn u32_unaligned() {
+        let data = [0x00, 0xab, 0xcd, 0xef, 0x01];
+        let view: &[u8] = &data[..];
+        let field = BEU32Field {
+            index: 1,
+        };
+        assert_eq!(Some(0xabcdef01), field.get(view));
+    }
+
+    #[test]
+    fn u32_invalid_address() {
+        let data = [0xab, 0xcd];
+        let view: &[u8] = &data[..];
+        let field = BEU16Field {
+            index: 1,
+        };
+        assert_eq!(None, field.get(view));
     }
 }
